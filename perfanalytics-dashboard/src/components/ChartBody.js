8930d623af9b23f9b32ba2b4c186f5642ba64284
Ceren BATTAL <cebattal@netas.com.tr>
2021-03-11 23:22:33 +0300

    ui improvements

--- perfanalytics-dashboard/src/components/ChartBody.js
+++ perfanalytics-dashboard/src/components/ChartBody.js
@@ -3,6 +3,7 @@ import Chart from './Chart';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
+import InfoCard from './InfoCard'
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -19,28 +20,37 @@ const useStyles = makeStyles((theme) => ({
 export default function ChartBody() {
     const classes = useStyles();
     return (
-        <Container maxWidth="m">
+        <Container maxWidth="sm">
             <div className={classes.root}>
                 <Grid container spacing={3}>
-                    <Grid item xs={6}>
-                    <Paper className={classes.paper}>
-                        <Chart />
+                    <Grid item xs={12}>
+                        <Paper className={classes.paper}>
+                        <InfoCard/>
                     </Paper>
                     </Grid>
-                    <Grid item xs={6}>
-                    <Paper className={classes.paper}>
-                        <Chart />
-                    </Paper>
+                    <Grid item xs={12}>
+                        <Paper className={classes.paper}>
+                            <h4>TTFB</h4>
+                            <Chart />
+                        </Paper>
                     </Grid>
-                    <Grid item xs={6}>
-                    <Paper className={classes.paper}>
-                        <Chart />
-                    </Paper>
+                    <Grid item xs={12}>
+                        <Paper className={classes.paper}>
+                            <h4>FCP</h4>
+                            <Chart />
+                        </Paper>
                     </Grid>
-                    <Grid item xs={6}>
-                    <Paper className={classes.paper}>
-                        <Chart />
-                    </Paper>
+                    <Grid item xs={12}>
+                        <Paper className={classes.paper}>
+                            <h4>Dom-Load</h4>
+                            <Chart />
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Paper className={classes.paper}>
+                            <h4>Window-Load</h4>
+                            <Chart />
+                        </Paper>
                     </Grid>
                 </Grid>
             </div>
